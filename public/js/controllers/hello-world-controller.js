b98f9618526410bd200cde5f4e76d93f095ebe5e
RarePep8 <alex.wong.utsc@gmail.com>
2018-02-24 15:24:54 -0500

    Added fetch functionality from reddit API

--- public/js/controllers/hello-world-controller.js
+++ public/js/controllers/hello-world-controller.js
@@ -1,4 +1,10 @@
 var app = angular.module('sampleApp', []);
-app.controller('HelloWorldController', function($scope) {
+app.controller('HelloWorldController', function($scope, $http) {
     $scope.greeting= "hi";
+    $scope.getRandomMeme = function(){
+    $http.get('https://www.reddit.com/r/me_irl/comments/7zxd9h/meirl/.json').
+        then(function(response) {
+            $scope.greeting = response.data;
+        });
+      }
 });
